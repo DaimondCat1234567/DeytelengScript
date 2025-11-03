73a84a40d6f80eb7537b1603abc125ff9b69fea2
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-03 17:57:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 docData = {}
 docData.input = document.getElementById('input')
 docData.output = document.getElementById('output')
+docData.css = document.getElementById('css')
+docData.theme = document.getElementById('theme')
 theme = localStorage.getItem('theme')
+if (!theme) {
+  theme = 'light'
+  localStorage.setItem(theme)
+}
 apps = localStorage.getItem('apps')
 app = {}
+themes = {
+  "light": "1",
+  "dark": "2",
+}
+themeIcon = {
+  "1": "/DeytelengScript/tema-1.svg",
+  "2": "/DeytelengScript/tema-2.svg",
+}
+themeStyle = {
+  "1": "/DeytelengScript/main.css",
+  "2": "/DeytelengScript/main2.css",
+}
+function updateTheme(th) {
+  docData.css = themeStyle[th]
+  docData.theme = themeIcon[th]
+}
+function newTheme() {
+  if (theme === 'light') {
+    theme = 'dark'
+    localStorage.setItem(theme)
+  } else {
+    theme = 'light'
+    localStorage.setItem(theme)
+  }
+  updateTheme(themes[theme])
+}
